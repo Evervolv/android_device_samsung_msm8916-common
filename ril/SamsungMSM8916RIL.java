b5daaa6cbefd85e23aa62a6e00d2969f75305e42
Davis Malnieks <daavis.90@gmail.com>
2016-01-10 14:20:37 +0200

    ril: use preferredNetworkType instead of networkModes
    Change-Id: Ic39f1dd16e63614965a2974c90a884170dd7c0f6

--- ril/SamsungMSM8916RIL.java
+++ ril/SamsungMSM8916RIL.java
@@ -101,8 +101,8 @@ public class SamsungMSM8916RIL extends RIL implements CommandsInterface {
 
     private AudioManager mAudioManager;
 
-    public SamsungMSM8916RIL(Context context, int networkModes, int cdmaSubscription) {
-        this(context, networkModes, cdmaSubscription, null);
+    public SamsungMSM8916RIL(Context context, int preferredNetworkType, int cdmaSubscription) {
+        this(context, preferredNetworkType, cdmaSubscription, null);
     }
 
     public SamsungMSM8916RIL(Context context, int preferredNetworkType,
