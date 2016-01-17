2ee4e7995560e8a48fd180658aaaf913841ab574
Davis Malnieks <daavis.90@gmail.com>
2016-01-17 11:30:49 +0200

    msm8916: squash updates
     camera: hfr support
     update media_codecs.xml
     add media_codecs_performance.xml
     update media_profiles.xml
    
    Change-Id: I39a70174c57d2213c164d48f050c4143616aa18c

--- msm8916.mk
+++ msm8916.mk
@@ -139,6 +139,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/samsung/msm8916-common/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    device/samsung/msm8916-common/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     device/samsung/msm8916-common/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Misc.
