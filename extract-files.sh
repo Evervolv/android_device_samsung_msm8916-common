4a6cf46befdcb19f677a0c1ce3fe48eff5dce483
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-03 22:54:48 -0400

    msm8916: Add repository setup scripts
    msm8916: Add board vendor check to vendor makefile
    
    msm8916: Append local path variable when generating vendor makefile
    
    setup-makefiles: Fix makefile generation
     * Add device list to makefile filters at generation instead of the
       board platform.
    
    Change-Id: Ib8fed16da38dbf03b0d8bda1f23dddc99ad67a6c
    
    setup-makefiles: add j7ltechn and j7ltespr to guard
    Change-Id: I4b9cc78c07fe806597bfb46815613f2b0db6b072
    
    msm8916: Update setup-makefiles and extract-files
    
     * Add support for specific section extraction
     * Update extract-files argument handling
     * Make spacing consistent
    
    Change-Id: I567764c29199256e175ec6a7794d755a38e908a8

---
+++ extract-files.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+function print_help() {
+    echo "Usage: `basename $0` [OPTIONS] "
+    echo "  -b | --setup-all  Set up all dirs (device and device-commons)"
+    echo "  -p | --path  Vendor blob source path (to ota package or system folder)"
+    echo "  -s | --section  Section to extract"
+    echo "  -c | --clean-vendor  Clean vendor dirs"
+    echo "  -h | --help Print this text"
+    exit 0
+}
+
+BOARD_COMMON=msm8916-common
+VENDOR=samsung
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+DEVICE_DIR="$MY_DIR"/../$DEVICE
+DEVICE_COMMON_DIR="$MY_DIR"/../$DEVICE_COMMON
+
+# determine which blob dirs to set up
+if [ -z "$SETUP_BOARD_COMMON_DIR" ]; then
+    SETUP_BOARD_COMMON_DIR=1
+fi
+
+if [ -z "$SETUP_DEVICE_DIR" ]; then
+    SETUP_DEVICE_DIR=0
+fi
+
+if [ -z "$SETUP_DEVICE_COMMON_DIR" ]; then
+    SETUP_DEVICE_COMMON_DIR=0
+fi
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# default to not sanitizing the vendor folder before extraction
+clean_vendor=false
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --setup-all )      SETUP_DEVICE_DIR=1
+                                SETUP_DEVICE_COMMON_DIR=1
+                                SETUP_BOARD_COMMON_DIR=1
+                                ;;
+        -p | --path )           shift
+                                SRC=$1
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                clean_vendor=false
+                                ;;
+        -c | --clean-vendor )   clean_vendor=true
+                                ;;
+        -h | --help )           print_help
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+if  [ "$SETUP_BOARD_COMMON_DIR" -eq 1 ]; then
+    # Initialize the helper for common
+    setup_vendor "$BOARD_COMMON" "$VENDOR" "$CM_ROOT" true $clean_vendor
+    extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+fi
+
+if [ "$SETUP_DEVICE_COMMON_DIR" -eq 1 ] && [ -s $DEVICE_COMMON_DIR/proprietary-files.txt ]; then
+    # Reinitialize the helper for device-common
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true $clean_vendor
+    extract $DEVICE_COMMON_DIR/proprietary-files.txt "$SRC" "$SECTION"
+fi
+
+if [ "$SETUP_DEVICE_DIR" -eq 1 ] && [ -s $DEVICE_DIR/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+    extract $DEVICE_DIR/proprietary-files.txt "$SRC" "$SECTION"
+fi
+
+"$MY_DIR"/setup-makefiles.sh
