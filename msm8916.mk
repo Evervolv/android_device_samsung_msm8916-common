91f0ebdf958560693301743d6a6266394bd24b7d
Mahantesh Sanakall <msanak@codeaurora.org>
2019-01-03 18:49:39 +0530

    msm8916: Do not enable broadcastradio by default
        - Remove broadcast radio entries from manifest.xml
          and msm8916.mk.
    
    Change-Id: Ibf0f52fb487512ced58903d5d2b0dae7d9c7254c

--- msm8916.mk
+++ msm8916.mk
@@ -30,7 +30,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8916 \
