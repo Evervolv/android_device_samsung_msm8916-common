8713e1b67074f2bdbb7ce3c5a200c1e8eaf181a2
Nick Reuter <nreuter85@gmail.com>
2019-03-08 16:31:56 -0600

    msm8916-common: drop legacy camera provider support
    Change-Id: I6d0a54aaaf7b25363d49b7a706afc21574b80411

--- msm8916.mk
+++ msm8916.mk
@@ -110,8 +110,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     libcamera_shim \
     libmm-qcamera \
     camera.msm8916
