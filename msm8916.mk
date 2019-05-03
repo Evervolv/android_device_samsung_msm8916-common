cb60e2ad4bd3d4d69db51a0ebb7e271ad107b662
Nick Reuter <nreuter85@gmail.com>
2019-05-02 23:40:45 -0500

    Revert "Add SELinux policy for clearkey HIDL service."
    This reverts commit e474815a4cd86aaf6343fa50d710f04763014523.

--- msm8916.mk
+++ msm8916.mk
@@ -162,8 +162,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.0-service
 
 # Ebtables
 PRODUCT_PACKAGES += \
