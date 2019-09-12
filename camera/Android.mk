3f6be0ca338f744cffb83c126012e4b3df90650f
Nick Reuter <nreuter85@gmail.com>
2019-09-12 03:08:26 -0500

    camera: update header libs
    Change-Id: I697470dc0459e28c3c72b713a34e396d2ccc3a0a

--- camera/Android.mk
+++ camera/Android.mk
@@ -3,19 +3,27 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_HEADER_LIBRARIES += \
-    libnativebase_headers
+    libnativebase_headers \
+    libnativewindow_headers
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl \
+    libhardware \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libdl \
     android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    android.hardware.graphics.bufferqueue@1.0 \
+    android.hardware.graphics.bufferqueue@2.0
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
