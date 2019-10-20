ce5f900dc0b6aef52dbab05651d5a0d63e153175
Ethan Chen <intervigil@gmail.com>
2019-10-19 23:53:57 -0700

    msm8916-common: Add shim for libbase LogMessage functions
    Change-Id: I4d1db75e5407586c9b69d94803af0ef9a9a91037
    Signed-off-by: Jens Lody <jens@jenslody.de>
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -239,7 +239,8 @@ TARGET_LD_SHIM_LIBS := \
     /vendor/lib/libsec-ril-dsds.so|libshim_secril.so \
     /vendor/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
     /vendor/lib/libizat_core.so|libshim_gps.so \
-    /vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
+    /vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /vendor/lib/hw/android.hardware.bluetooth@1.0-impl-qti.so|libbase_shim.so
 
 # Snapdragon LLVM
 TARGET_USE_SDCLANG := true
