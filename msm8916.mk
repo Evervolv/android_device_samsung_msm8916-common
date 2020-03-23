4f20e4d944e27c9c5973632bd09525ce8284e9c9
Bruno Martins <bgcngm@gmail.com>
2020-03-23 12:16:29 +0100

    msm8916-common: Convert lights HAL into a native binderized HAL
    Change-Id: Ie729c67a71d59ea004255f454b41918bf41716d7
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    
    msm8916-common: simplify Lights HAL
    
    Now only managing the backlight
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I3cdbbb4eae7b6ef6a880b3ff2764be100e1d4498
    
    msm8916-common: Remove unused function definition from lights HAL.
    
    Signed-off-by: Jens Lody <jens@jenslody.de>
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I0e2c54f9b3db180327013d43433c47f1fbced5bc

--- msm8916.mk
+++ msm8916.mk
@@ -273,9 +273,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8916
+    android.hardware.light@2.0-service.msm8916
 
 # Media configurations
 ifeq ($(filter j7ltespr j7ltechn,$(TARGET_DEVICE)),)
