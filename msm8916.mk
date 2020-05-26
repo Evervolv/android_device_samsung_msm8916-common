0bb4e126c81b0f886e3d3146d0bbc1c896a70cb9
Wang Han <416810799@qq.com>
2020-05-26 20:58:17 +0800

    msm8916-common: Switch to TimeKeep
     * SE policies are imported and modified from
       https://github.com/sonyxperiadev/device-sony-sepolicy.
       Modifications are needed because qcom legacy policy
       contains conflicting labels and rules.
    
    Change-Id: Id04a824dea69976f6fc9d48bef77859cc82971ed

--- msm8916.mk
+++ msm8916.mk
@@ -471,6 +471,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Time services
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
