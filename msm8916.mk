302cc5c217f5c195d67c9e50ac177c695f222e62
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    Add prebuilt protobuf from sdk29
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- msm8916.mk
+++ msm8916.mk
@@ -397,6 +397,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+# Protobuf
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # Radio
 PRODUCT_PACKAGES += \
     librmnetctl \
