c7d38cb669b08ac2da14a27e4c3ea4515627ffaa
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    nsn8916-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- msm8916.mk
+++ msm8916.mk
@@ -399,7 +399,7 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Protobuf
 PRODUCT_COPY_FILES += \
