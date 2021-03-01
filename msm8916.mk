c87d6244f3e97826d55db8e476a9f7f88e96b58a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-01 21:22:50 +0100

    msm8916-common: Binderize sensors HIDL
    Change-Id: Ib22c560259a88f3d00fc35a4da7972fd7c25b00c

--- msm8916.mk
+++ msm8916.mk
@@ -456,6 +456,7 @@ PRODUCT_COPY_FILES += \
 # Sensor HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     sensors.msm8916
 
 # Thermal
