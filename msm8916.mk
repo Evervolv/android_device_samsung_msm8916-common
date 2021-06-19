9cadef7180f2393f8a116f4de6a367c48028e51d
Nick Reuter <nreuter85@gmail.com>
2021-06-18 21:15:16 -0500

    Revert "msm8916-common: Binderize sensors HIDL"
    This reverts commit c87d6244f3e97826d55db8e476a9f7f88e96b58a.

--- msm8916.mk
+++ msm8916.mk
@@ -459,7 +459,6 @@ PRODUCT_COPY_FILES += \
 # Sensor HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     sensors.msm8916
 
 # Thermal
